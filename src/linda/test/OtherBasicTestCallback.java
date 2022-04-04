a058bb5a3b7156fcc3522505045c7f7e7c076284
Tom Heurtebise <91341903+theurt@users.noreply.github.com>
2022-04-04 22:12:23 +0200

    interblocage fixed

--- src/linda/test/OtherBasicTestCallback.java
+++ src/linda/test/OtherBasicTestCallback.java
@@ -13,6 +13,7 @@ public class OtherBasicTestCallback {
     private static Linda linda;
     private static Tuple cbmotif;
     
+    /*Callback qui se recharge */
     private static class MyCallback implements Callback {
         public void call(Tuple t) {
             System.out.println("CB got "+t);
@@ -36,22 +37,22 @@ public class OtherBasicTestCallback {
         linda.write(t3);
         
         cbmotif = new Tuple(Integer.class, String.class);
-        linda.eventRegister(eventMode.TAKE, eventTiming.IMMEDIATE, cbmotif, new MyCallback());
+        linda.eventRegister(eventMode.TAKE, eventTiming.IMMEDIATE, cbmotif, new MyCallback()); //Lance l'event puisque immediat
+        
+        linda.debug("(2)");
         
         Tuple t4 = linda.tryRead(t3);
-        System.out.println("(2) TryRead: " + t4);	//renvoit null car eventRegister l'a retiré immédiatement
+        System.out.println("(3) TryRead: " + t4);	//renvoit null car eventRegister l'a retiré immédiatement
         
         Tuple t1 = new Tuple(4, 5);
-        System.out.println("(2) write: " + t1);
+        System.out.println("(4) write: " + t1);
         linda.write(t1);
 
         Tuple t2 = new Tuple("hello", 15);
-        System.out.println("(2) write: " + t2);
+        System.out.println("(5) write: " + t2);
         linda.write(t2);
-        linda.debug("(2)");
+        linda.debug("(5)");
 
-
-        linda.debug("(2)");
     }
     	
     public static void testFuture() {
@@ -66,27 +67,25 @@ System.out.println("\n----------------TESTS FUTURE----------------\n");
         linda.write(t3);
         
         cbmotif = new Tuple(Integer.class, String.class);
-        linda.eventRegister(eventMode.TAKE, eventTiming.FUTURE, cbmotif, new MyCallback());
+        linda.eventRegister(eventMode.TAKE, eventTiming.FUTURE, cbmotif, new MyCallback()); //Ne doit rien faire
 
         Tuple t4 = linda.tryRead(t3);
-        System.out.println("(2) TryRead: " + t4);
+        System.out.println("(3) TryRead: " + t4);	//Renvoit 4 foo
         
         Tuple t1 = new Tuple(4, 5);
-        System.out.println("(2) write: " + t1);
+        System.out.println("(4) write: " + t1);
         linda.write(t1);
 
         Tuple t2 = new Tuple("hello", 15);
-        System.out.println("(2) write: " + t2);
+        System.out.println("(5) write: " + t2);
         linda.write(t2);
         
-        Tuple t5 = new Tuple(4, "bonsoir");
-        System.out.println("(2) write: " + t5);
+        Tuple t5 = new Tuple(4, "bonsoir");			//Doit lancer l'event
+        System.out.println("(6) write: " + t5);
         linda.write(t5);
-        System.out.println(linda.tryTake(cbmotif));
-        System.out.println(linda.tryTake(cbmotif));
-        linda.debug("(2)");
-
-        linda.debug("(2)");
+        System.out.println(linda.tryTake(cbmotif)); //null car tous les tuples correspondant ont ete pris
+        System.out.println(linda.tryTake(cbmotif));	//idem
+        linda.debug("(7)");
     }
     
     public static void main(String[] a) {
